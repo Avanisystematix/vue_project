245ba6ec2938ad10578f648acb88a248c070f9db
Avanisystematix <avani.vindhyawasiny@systematixindia.com>
2023-03-28 12:05:37 +0530

    fisrt add

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home data="hey"/>
+    <ComposingWithComponent/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import Home from './components/Home.vue'
+import ComposingWithComponent from './components/ComposingWithComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    // Home,
+    ComposingWithComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="main">
+    <h1>hello</h1> 
+    <!-- to pass data -->
+    <!-- <h1>{{ data }}</h1> -->
+
+    <!-- events -->
+    <!-- <button v-on:click="hello">Submit</button>  -->
+
+    <!-- conditions -->
+    <!-- <h1 v-if="show">conditional rendring if</h1>
+    <h1 v-else>conditional rendring else</h1>
+    <button v-on:click="display">Toggle element</button> -->
+
+    <!-- loops -->
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+name: "Home",
+
+// to pass data
+props:{
+  data:String
+},
+
+// events
+// methods:{
+//   hello() {
+//      console.log("hello");
+//   },
+//   hello(data){
+//     alert(data)
+//   }
+// },
+
+//loops
+data(){
+  return {
+    users:[
+      {id:2,name:"test2",email:"test2@test.com"},
+      {id:3,name:"test3",email:"test3@test.com"},
+      {id:4,name:"test4",email:"test4@test.com"},
+    ]
+  }
+}
+
+}
+</script>
+
+<style>
+
+</style>
